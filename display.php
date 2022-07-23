72a64011c9dc95ab023a2ffeeb1626f76618895a
Raphael Darley <raphael@raphaeldarley.com>
2022-07-23 01:26:49 +0100

    add multiple search and add only image checkbox

--- display.php
+++ display.php
@@ -24,20 +24,27 @@ try {
 
 $page_num = default_val($_REQUEST["page"], 1);
 
-$num_per_page = 50;
+$num_per_page = 500;
 // var_dump($_REQUEST);
 // var_dump($_REQUEST["search"]);
 
 
-if ($_REQUEST["search"] == NULL) {
+if ($_REQUEST["search"] == NULL) { // no search terms
   $get_stmt = $pdo->prepare("SELECT * FROM jlh_papers LIMIT ?, $num_per_page");
   $get_stmt->execute([($page_num - 1) * $num_per_page]);
-} else {
+} elseif ($_REQUEST["search2"] == NULL) { // only one search
   $start = ($page_num - 1) * $num_per_page;
   $search_term = $_REQUEST["search"];
   $get_stmt = $pdo->prepare("SELECT * FROM jlh_papers WHERE (page_text LIKE '%$search_term%') LIMIT ?, $num_per_page");
   // $get_stmt->debugDumpParams();
 
+  $get_stmt->execute([$start]);
+} else {
+  $start = ($page_num - 1) * $num_per_page;
+  $search_term = $_REQUEST["search"];
+  $search_term2 = $_REQUEST["search2"];
+  $get_stmt = $pdo->prepare("SELECT * FROM jlh_papers WHERE (page_text LIKE '%$search_term%' AND page_text LIKE '%$search_term2%') LIMIT ?, $num_per_page");
+  // $get_stmt->debugDumpParams();
   $get_stmt->execute([$start]);
 }
 
@@ -50,6 +57,7 @@ $rows = $get_stmt->fetchAll();
 </head>
 
 <body>
+
   <a href="?page=<?php echo $page_num - 1 ?><?php if ($_REQUEST["search"] != NULL) {
                                               echo "&search=" . $_REQUEST["search"];
                                             } ?>" <?php if ($page_num < 2) {
@@ -58,8 +66,34 @@ $rows = $get_stmt->fetchAll();
   <a href="?page=<?php echo $page_num + 1 ?><?php if ($_REQUEST["search"] != NULL) {
                                               echo "&search=" . $_REQUEST["search"];
                                             } ?>">Next</a>
+
+
+  <?php
+  // $previous_status = "";
+  // if ($page_num < 2) {
+  //   $previous_status = 'style="pointer-events: none"';
+  // }
+  // if ($_REQUEST["search"] != NULL && $_REQUEST["search2"] != NULL) {
+  //   echo '<a href="?page=' . $page_num - 1 . '&search=' . $_REQUEST["search"] . '&search2=' . $_REQUEST["search2"] . '"' . $previous_status . '>Previous</a>';
+  // } else if ($_REQUEST["search"] != NULL) {
+  //   echo "test";
+  // } else {
+  //   echo "test";
+  // }
+  ?>
+
+
   <form method="get">
-    <input type="text" name="search">
+    <input type="text" name="search" <?php if (isset($search_term)) {
+                                        echo "value=" . $search_term;
+                                      } ?>>
+    <input type="text" name="search2" <?php if (isset($search_term2)) {
+                                        echo "value=" . $search_term2;
+                                      } ?>>
+    <label for="only-images">only images</label>
+    <input type="checkbox" name="only-images" <?php if ($_REQUEST["only-images"]) {
+                                                echo "checked";
+                                              } ?>>
     <button type="submit">submit</button>
   </form>
   <table>
@@ -72,10 +106,15 @@ $rows = $get_stmt->fetchAll();
     <?php foreach ($rows as $db_row) {
       $id = $db_row["id"];
       $image_path = $db_row["image_path"];
-      $img_tag = "<img src=\"$image_path\" width=\"512\">";
       $page_text = $db_row["page_text"];
       $img_text = $db_row["img_text"];
-      $html_row = "<tr><td>$id</td><td>$img_tag</td><td>$page_text</td><td>$img_text</td></tr>";
+      if (!$_REQUEST["only-images"]) {
+        $img_tag = "<img src=\"$image_path\" width=\"512\">";
+        $html_row = "<tr><td>$id</td><td>$img_tag</td><td>$page_text</td><td>$img_text</td></tr>";
+      } else {
+        $img_tag = "<img src=\"$image_path\" width=\"768\">";
+        $html_row = "<tr><td>$id</td><td>$img_tag</td></tr>";
+      }
       echo $html_row;
     }
     ?>
